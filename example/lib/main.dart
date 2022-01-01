854e4cdb1e2781db489843354d1ddf38e1251786
egyleader <ahmed.os.attya@gmail.com>
2022-01-01 12:12:35 +0200

    version 0.1.0 ready

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_audio_player/helpers/init_just_audio_background.dart';
 import 'package:flutter_audio_player/models/notification_configuration.dart';
+import 'package:flutter_audio_player/widgets/players/basic_audio_player.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:audio_service/audio_service.dart';
 import 'package:flutter_audio_player/widgets/players/full_audio_player.dart';
@@ -21,7 +22,7 @@ class HomeScreen extends StatelessWidget {
     AudioSource.uri(Uri.parse('https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3'),
         tag: MediaItem(
             id: '1',
-            artUri: Uri.parse('https://picsum.photos/id/237/200/300'),
+            artUri: Uri.parse('https://picsum.photos/300/300'),
             title: 'Audio Title ',
             album: 'amazing album'))
   ]);
@@ -33,7 +34,7 @@ class HomeScreen extends StatelessWidget {
             child: Padding(
                 padding: const EdgeInsets.all(20.0),
                 child: Center(
-                  child: FullAudioPlayer(autoPlay: false, playlist: _playlist),
+                  child: BasicAudioPlayer(playlist: _playlist),
                 ))));
   }
 }
