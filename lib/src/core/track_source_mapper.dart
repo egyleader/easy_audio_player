f83148f21b3f1b43a0a31fa3baa00b7b975889f7
osos <ahmed.os.attya@gmail.com>
2026-03-24 07:42:21 +0200

    feat: add TrackSourceMapper (AudioTrack → just_audio AudioSource)

---
+++ lib/src/core/track_source_mapper.dart
@@ -0,0 +1,36 @@
+import 'package:just_audio/just_audio.dart';
+import 'package:just_audio_background/just_audio_background.dart';
+
+import '../models/audio_track.dart';
+
+/// Internal: converts [AudioTrack] into a just_audio [AudioSource] with a
+/// [MediaItem] tag for background notification display.
+/// Not exported publicly.
+class TrackSourceMapper {
+  static AudioSource toAudioSource(AudioTrack track) {
+    final tag = MediaItem(
+      id: track.id,
+      title: track.title,
+      artist: track.artist,
+      album: track.album,
+      artUri: track.artworkUrl != null ? Uri.parse(track.artworkUrl!) : null,
+      duration: track.duration,
+      extras: track.extras,
+    );
+
+    return switch (track.sourceType) {
+      TrackSourceType.network => AudioSource.uri(
+          Uri.parse(track.url!),
+          tag: tag,
+        ),
+      TrackSourceType.file => AudioSource.uri(
+          track.file!.uri,
+          tag: tag,
+        ),
+      TrackSourceType.asset => AudioSource.asset(
+          track.assetPath!,
+          tag: tag,
+        ),
+    };
+  }
+}
